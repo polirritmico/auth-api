5d79aa6e5cf5d634a140627b53d882a938b96792
polirritmico <ebray187@gmail.com>
2026-05-16 17:57:45 -0400

    feat(AuthResponse): add dto Auth response

---
+++ src/main/java/cl/duoc/auth/dto/response/AuthResponse.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
