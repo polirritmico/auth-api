be1aa4c053a0d6e32cee37f7dcf4080b9759ccdd
polirritmico <ebray187@gmail.com>
2026-05-16 17:59:12 -0400

    feat(JwtService): add base class & token generation approach

---
+++ src/main/java/cl/duoc/auth/service/JwtService.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.auth.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import java.time.Instant;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    private static final String ROLE = "role";
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    @Value("${jwt.issuer}")
+    private String issuer;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateToken(String user, String role) {
+        Instant now = Instant.now();
+        return JWT.create()
+                .withSubject(user)
+                .withIssuer(issuer)
+                .withClaim(ROLE, role)
+                .withIssuedAt(now)
+                .withExpiresAt(now.plusMillis(expiration))
+                .sign(Algorithm.HMAC256(secret));
+    }
+}
