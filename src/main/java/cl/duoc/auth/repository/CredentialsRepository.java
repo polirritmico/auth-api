46d74128b8d1ceda7e8de62eb8072e0c53c9fd40
polirritmico <ebray187@gmail.com>
2026-05-16 17:55:13 -0400

    feat(CredentialsRepository): add base repository

---
+++ src/main/java/cl/duoc/auth/repository/CredentialsRepository.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.auth.repository;
+
+import cl.duoc.auth.model.Credentials;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CredentialsRepository extends JpaRepository<Credentials, Long> {
+    Optional<Credentials> findByUserIdAndRevokedAtIsNull(String userId);
+
+    List<Credentials> findByRevokedAtIsNull();
+
+    default List<Credentials> findAll() {
+        return findByRevokedAtIsNull();
+    }
+
+    default Optional<Credentials> findByUserId(String userId) {
+        return findByUserIdAndRevokedAtIsNull(userId);
+    }
+}
