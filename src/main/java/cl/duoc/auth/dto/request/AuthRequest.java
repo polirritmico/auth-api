6245001ec4ea99f839926082bce38ea56a5791e0
polirritmico <ebray187@gmail.com>
2026-05-16 17:57:15 -0400

    feat(AuthRequest): add dto authentication request

---
+++ src/main/java/cl/duoc/auth/dto/request/AuthRequest.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequest {
+    @NotBlank(message = "El usuario es obligatorio")
+    private String user;
+
+    @NotBlank(message = "El password es obligatorio")
+    private String password;
+}
