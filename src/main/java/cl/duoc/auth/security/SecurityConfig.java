b691322f497eb9f73087466d8a13330d46aa575c
polirritmico <ebray187@gmail.com>
2026-05-31 19:53:56 -0400

    feat(security): add permissions & role checks for new endpoints

--- src/main/java/cl/duoc/auth/security/SecurityConfig.java
+++ src/main/java/cl/duoc/auth/security/SecurityConfig.java
@@ -6,6 +6,7 @@
  */
 package cl.duoc.auth.security;
 
+import cl.duoc.auth.exception.ForbiddenAccessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,16 +17,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
+    private final HandlerExceptionResolver handlerExceptionResolver;
 
     @Value("${jwt.public}")
     private String[] publicPaths;
 
     private final String publicAuthEndpoint = "/api/v1/auth";
+    private final String changeOwnPasswordEndpoint = publicAuthEndpoint + "/me/password";
     private final String allowedRole = "admin";
 
     @Bean
@@ -37,12 +41,16 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, publicAuthEndpoint)
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, publicAuthEndpoint)
                         .permitAll()
                         .requestMatchers(publicPaths)
                         .permitAll()
+                        .requestMatchers(HttpMethod.PUT, changeOwnPasswordEndpoint)
+                        .authenticated()
                         .anyRequest()
                         .hasAuthority(allowedRole))
+                .exceptionHandling(ex -> ex.accessDeniedHandler((req, res, accessDeniedException) ->
+                        handlerExceptionResolver.resolveException(req, res, null, new ForbiddenAccessException())))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
