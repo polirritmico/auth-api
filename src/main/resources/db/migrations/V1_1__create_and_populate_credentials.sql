fc312ea02ce5ee6322eb2078664e6c42c7ec47e0
polirritmico <ebray187@gmail.com>
2026-05-31 19:49:54 -0400

    chore(migrations): change updated_at column definition to NOT NULL

--- src/main/resources/db/migrations/V1_1__create_and_populate_credentials.sql
+++ src/main/resources/db/migrations/V1_1__create_and_populate_credentials.sql
@@ -33,7 +33,7 @@ CREATE TABLE `credentials` (
   `password` varchar(255) NOT NULL,
   `role` varchar(255) NOT NULL,
   `created_at` datetime(6) NOT NULL,
-  `updated_at` datetime(6) DEFAULT NULL,
+  `updated_at` datetime(6) NOT NULL,
   `revoked_at` datetime(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
