63fb6db474fceb74b12d3cf9352af5df16c35a8c
polirritmico <ebray187@gmail.com>
2026-05-31 16:24:34 -0400

    feat(SecurityConfig): add allowed role validation & refactor

--- src/main/java/cl/duoc/auth/security/SecurityConfig.java
+++ src/main/java/cl/duoc/auth/security/SecurityConfig.java
@@ -10,30 +10,40 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Value("${jwt.public}")
+    private String[] publicPaths;
+
+    private final String publicAuthEndpoint = "/api/v1/auth";
+    private final String allowedRole = "admin";
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Value("${jwt.public}")
-    private String[] publicPaths;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers(publicPaths)
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, publicAuthEndpoint)
+                        .permitAll()
+                        .requestMatchers(publicPaths)
                         .permitAll()
                         .anyRequest()
-                        .authenticated())
+                        .hasAuthority(allowedRole))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
