16751bcb6ed16cee6b545dce2a9f6e2459630d37
polirritmico <ebray187@gmail.com>
2026-06-09 19:29:09 -0400

    docs(StatusController): add check health swagger documentation

--- src/main/java/cl/duoc/auth/controller/StatusController.java
+++ src/main/java/cl/duoc/auth/controller/StatusController.java
@@ -7,6 +7,9 @@
 package cl.duoc.auth.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Map;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Tag(name = "System", description = "Service monitoring & status")
 public class StatusController {
-
     @GetMapping("/api/v1/health")
     @Operation(summary = "Check service health", description = "Checks the microservice avaliability")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Successful response",
+            content = @Content(mediaType = "application/json", examples = @ExampleObject(value = """
+                    {
+                        "status": "health state",
+                        "service": "Microservice name"
+                    }
+                    """)))
     public ResponseEntity<Map<String, String>> checkHealth() {
         return ResponseEntity.ok(Map.of("status", "OK", "service", "AuthMicroservice"));
     }
