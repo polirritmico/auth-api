df5e8da106b3a576d61dafd71aa0212ed712753b
polirritmico <ebray187@gmail.com>
2026-06-09 19:29:55 -0400

    docs(ApiErrorResponse): add schema annotations for exceptions dto

--- src/main/java/cl/duoc/auth/exception/dto/ApiErrorResponse.java
+++ src/main/java/cl/duoc/auth/exception/dto/ApiErrorResponse.java
@@ -6,6 +6,7 @@
  */
 package cl.duoc.auth.exception.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,11 +17,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(name = "ApiErrorResponse", description = "Standaridized error response payload")
 public class ApiErrorResponse {
+
+    @Schema(description = "Time the error occurred", example = "2026-06-09T17:07:23")
     private LocalDateTime timestamp;
+
+    @Schema(description = "HTTP status code", example = "400")
     private int status;
+
+    @Schema(description = "HTTP error description", example = "Bad Request")
     private String error;
+
+    @Schema(description = "Detailed error message", example = "El usuario es obligatorio")
     private String message;
+
+    @Schema(description = "Endpoint where the error occurred", example = "/api/v1/auth")
     private String path;
+
+    @Schema(description = "Exception class name", example = "MethodArgumentNotValidException")
     private String kind;
 }
