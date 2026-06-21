3dda0470b7d3aba8811fcb3314b7d3688628dfbd
polirritmico <ebray187@gmail.com>
2026-06-20 23:26:28 -0400

    feat(eureka): add client discovery & properties settings

--- src/main/java/cl/duoc/auth/AuthApplication.java
+++ src/main/java/cl/duoc/auth/AuthApplication.java
@@ -9,8 +9,10 @@ package cl.duoc.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
